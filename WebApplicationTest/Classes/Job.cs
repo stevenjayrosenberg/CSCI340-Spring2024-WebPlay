aef3e0a72cc9f2a35e6929e5fec3a1636d8c3f1c
Karma Gurung <kgurung4@uwsuper.edu>
2024-03-06 10:33:58 -0600

    Added some instace variables and get methods in the Job class

--- WebApplicationTest/Classes/Job.cs
+++ WebApplicationTest/Classes/Job.cs
@@ -1,1 +1,29 @@
 ﻿
+namespace WebApplicationTest
+{
+    public class Job
+    {
+        private string Name;
+        private int Desiredhours;
+        private string Department;
+
+        public  Job(string JobName, int theDesiredHours, string DepartmentName)
+        {
+            Name = JobName;
+            Desiredhours = theDesiredHours;
+            Department = DepartmentName;
+        }
+        public string getJobName()
+        {
+            return Name;
+        }
+        public int getDesiredHours()
+        {
+            return Desiredhours;
+        }
+        public string getDepartmentName()
+        {
+            return Department;
+        }
+    }
+}
