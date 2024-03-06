553b63250a0063eb86c6039d975cff0692d37044
varjaktp <busolaorelelewa@gmail.com>
2024-03-06 11:16:25 -0600

    Added Administrator constructor

--- WebApplicationTest/Classes/Administrator.cs
+++ WebApplicationTest/Classes/Administrator.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+
+namespace WebApplicationTest
+{
+    public class Administrator : User
+    {
+        public Administrator(String AdminName, String AdminEmail, String AdminEmployeeId) :
+            base(AdminName, AdminEmail, AdminEmployeeId)
+        { }
+        override
+    }
+}
