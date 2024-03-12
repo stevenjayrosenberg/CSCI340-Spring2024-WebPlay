23150c9c0a51325e4c92d626b1c98d7731a75329
Jinah Yoo <gina07099@gmail.com>
2024-03-11 23:30:11 -0500

    added WorksFor and Job sql script

--- sqlScripts_SESSystem/tables.sql
+++ sqlScripts_SESSystem/tables.sql
@@ -1,19 +1,27 @@
-drop table if exists Team, Department, Supervises, [Admin], Supervisor, SE, [User]
-
-create database SESSystemTest
+drop table if exists Team, Job, Department, Supervises, [Admin], Supervisor, SE, [User], Works_for
+--create database SESSystemTest
 
 --Department table
-create table Department(
-department_name varchar(50) not null,
-constraint PK_Department primary key (department_name))
+CREATE TABLE Department(
+department_name VARCHAR(50),
+CONSTRAINT PK_Department PRIMARY KEY (department_name))
 
 --Team table
-create table Team(
-team_name varchar(50) not null,
-department_name varchar(50) not null,
-constraint PK_Team primary key (team_name),
-constraint FK_Team_Department foreign key (department_name)
-references Department(department_name))
+CREATE TABLE Team(
+team_name VARCHAR(50),
+department_name VARCHAR(50),
+CONSTRAINT PK_Team PRIMARY KEY (team_name, department_name),
+CONSTRAINT FK_Team_Department FOREIGN KEY (department_name)
+	REFERENCES Department(department_name))
+
+
+--Job table
+CREATE TABLE Job(
+job_title VARCHAR(50),
+department_name VARCHAR(50),
+CONSTRAINT PK_Job PRIMARY KEY (job_title, department_name),
+CONSTRAINT FK_Job_Department FOREIGN KEY (department_name)
+	REFERENCES Department(department_name))
 
 --User table
 CREATE TABLE [User]
@@ -25,6 +33,20 @@ username VARCHAR(10),
 email VARCHAR(20),
 CONSTRAINT PK_User PRIMARY KEY ([employee_id])
 )
+--Works_for table
+CREATE TABLE Works_for(
+user_employee_id CHAR(8),
+job_title VARCHAR(50),
+team_name VARCHAR(50),
+department_name VARCHAR(50),
+desired_hours int,
+CONSTRAINT PK_Works_for PRIMARY KEY (user_employee_id, job_title, team_name, department_name, desired_hours),
+CONSTRAINT FK_Works_for_User FOREIGN KEY (user_employee_id)
+	REFERENCES [User](employee_id),
+CONSTRAINT FK_Works_for_Team FOREIGN KEY (team_name, department_name)
+	REFERENCES [Team](team_name, department_name),
+CONSTRAINT FK_Works_for_Job FOREIGN KEY (job_title, department_name)
+	REFERENCES [Job] (job_title, department_name))
 
 --Admin table
 CREATE TABLE [Admin]
