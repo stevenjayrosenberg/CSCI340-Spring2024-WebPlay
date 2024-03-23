46738a7e426efda8ee83416314bcf97dd1b6b4ba
Jinah Yoo <gina07099@gmail.com>
2024-03-23 14:20:38 -0500

    test

--- sqlScripts_SESSystem/tables.sql
+++ sqlScripts_SESSystem/tables.sql
@@ -14,7 +14,6 @@ CONSTRAINT PK_Team PRIMARY KEY (team_name, department_name),
 CONSTRAINT FK_Team_Department FOREIGN KEY (department_name)
 	REFERENCES Department(department_name))
 
-
 --Job table
 CREATE TABLE Job(
 job_title VARCHAR(50),
@@ -33,6 +32,7 @@ username VARCHAR(10),
 email VARCHAR(20),
 CONSTRAINT PK_User PRIMARY KEY ([employee_id])
 )
+
 --Works_for table
 CREATE TABLE Works_for(
 user_employee_id CHAR(8),
@@ -40,7 +40,7 @@ job_title VARCHAR(50),
 team_name VARCHAR(50),
 department_name VARCHAR(50),
 desired_hours int,
-CONSTRAINT PK_Works_for PRIMARY KEY (user_employee_id, job_title, team_name, department_name, desired_hours),
+CONSTRAINT PK_Works_for PRIMARY KEY (user_employee_id, job_title, team_name, department_name),
 CONSTRAINT FK_Works_for_User FOREIGN KEY (user_employee_id)
 	REFERENCES [User](employee_id),
 CONSTRAINT FK_Works_for_Team FOREIGN KEY (team_name, department_name)
